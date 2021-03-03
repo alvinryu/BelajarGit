d7521b9e196cb9f029620cef342224c3ffcbdb04
Chrysler Imanuel <chryslerimanuel@gmail.com>
2021-03-03 18:05:24 +0700

    Commit dari cmd

---
+++ API/Repository/GeneralDapperRepository.cs
@@ -0,0 +1,37 @@
+﻿using API.Context;
+using API.Repository.Interface;
+using Dapper;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Repository
+{
+    public class GeneralDapperRepository<Entity> : IDapperRepository<Entity>
+         where Entity : class
+    {
+        private readonly IConfiguration _config;
+        private readonly IDbConnection _connection;
+
+        public GeneralDapperRepository(IConfiguration config) 
+        {
+            _config = config; 
+            _connection = new SqlConnection(_config.GetConnectionString("MyContext"));
+        }
+
+        public Entity ExecSP(string spName, DynamicParameters parameters = null)
+        {
+            return _connection.Query<Entity>(spName, parameters, commandType: CommandType.StoredProcedure).FirstOrDefault();
+        }
+
+        public IEnumerable<Entity> ExecSPList(string spName, DynamicParameters parameters = null)
+        {
+            return _connection.Query<Entity>(spName, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
---
+++ API/ViewModels/LoginVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.ViewModels
+{
+    public class LoginVM
+    {
+        public string Email { get; set; }
+
+        public string Password { get; set; }
+
+        public string FullName { get; set; }
+
+        public IEnumerable<string> Roles { get; set; } //buat nampilin di json 
+
+        public string RoleName { get; set; } //nama kolom di sp
+
+    }
+}
